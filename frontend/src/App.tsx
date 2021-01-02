b959659d27cc6306df57ad779d8182659a376fb0
Narasimha <narasimhav@virtuositycg.com>
2021-01-02 23:14:48 +0530

    react-bootstrap setup, hedaer and footer components setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Header, Footer } from './components';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
 	return (
 		<>
-			<h1>Welcome to pro shop</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to pro shop</h1>
+				</Container>
+			</main>
+			<Footer />
 		</>
 	);
 };
