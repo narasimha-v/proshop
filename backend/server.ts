675baf536df75cdd3a783958607db61ab4920376
Narasimha <narasimhav@virtuositycg.com>
2021-01-25 20:45:58 +0530

    production dependencies

--- backend/server.ts
+++ backend/server.ts
@@ -19,7 +19,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.json({ message: 'api is responding' }));
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -28,6 +27,14 @@ app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID!)
 );
 const dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(dirname, '/frontend/build')));
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => res.json({ message: 'api is responding' }));
+}
 app.use('/uploads', express.static(path.join(dirname, '/uploads')));
 app.use(notFound);
 app.use(errorHandler);
